b65891d2f78c8a55f00e37b65da6fa5da069f0eb
xaby19 <112349811+xaby19@users.noreply.github.com>
2022-10-01 10:33:22 +0200

    Add files via upload

--- programacion_estructurada/tema 1/ejercicio6/src/Main.java
+++ programacion_estructurada/tema 1/ejercicio6/src/Main.java
@@ -1,16 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println();
-        int numFilas = 9;
-        for (int altura = 1; altura <=numFilas ; altura++) {
-            for (int i = 0; i <= numFilas - altura ; i++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= (altura * 2 ) - 1 ; j++) {
 
-                System.out.print("*");
-            }
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+            Ejemplo n = 5
+             _____*
+             ____***
+             ___*****
+             __*******
+             _*********
+             ***********
+         */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Dime un número y te pinto una pirámide de ese tamaño: ");
+        int n = scanner.nextInt();
+        int rows = n + 1;
+
+        for (int row = 0; row < rows; row++) {
+            printSpaces(n, row);
+            printStars(row);
             System.out.println();
         }
+    }
+
+    private static void printSpaces(int n, int row) {
+        System.out.print(" ".repeat(n - row));
+    }
+
+    private static void printStars(int row) {
+        System.out.print("*".repeat(row) +  "*".repeat(row));
     }
 }
