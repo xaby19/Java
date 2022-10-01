4fb4312169ffe6265566740c57d05f2b988a6f61
xaby19 <112349811+xaby19@users.noreply.github.com>
2022-10-01 14:32:04 +0200

    Add files via upload

--- programacion_estructurada/tema 4/ejercicio 7/src/Main.java
+++ programacion_estructurada/tema 4/ejercicio 7/src/Main.java
@@ -1,18 +1,41 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        /*
+            Ejemplo n = 5
+             _____*
+             ____*_*
+             ___*___*
+             __*_____*
+             _*_______*
+             ***********
+         */
+
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Dame un entero y te pinto una pirámide de ese tamaño: ");
+        int n = scanner.nextInt();
 
-        System.out.println("Dime la primera nota");
-        double nota1 = Double.parseDouble(scanner.next());
-        System.out.println("Dime la otra nota");
-        double nota2 = Double.parseDouble(scanner.next());
-        System.out.println("Dime la utima nota");
-        double nota3 = Double.parseDouble(scanner.next());
-        double numeroNotas = 3;
-        double result = (nota1 + nota2 + nota3) / numeroNotas;
+        // First row
+        System.out.println(" ".repeat(n) + "*");
 
-        System.out.println("la media de las notas es " + result);
+        for (int row = 1; row < n; row++) {
+            printLeftSpaces(n, row);
+            printContent(row);
+            System.out.println();
+        }
+
+        // Last row
+        System.out.println("*".repeat((n * 2 ) + 1));
     }
+
+    private static void printLeftSpaces(int n, int row) {
+        System.out.print(" ".repeat(n - row));
+    }
+
+    private static void printContent(int row) {
+        System.out.print("*" + " ".repeat((row * 2) - 1) + "*");
+    }
+
 }
