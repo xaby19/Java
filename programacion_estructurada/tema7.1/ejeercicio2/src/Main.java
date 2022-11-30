77617015b7f6e8fd31f3203c7de311eb45ec02e6
xaby19 <112349811+xaby19@users.noreply.github.com>
2022-11-30 12:08:33 +0100

    Add files via upload

--- programacion_estructurada/tema7.1/ejeercicio2/src/Main.java
+++ programacion_estructurada/tema7.1/ejeercicio2/src/Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+/*
+Escribe un programa que pida 20 números enteros. Estos números se deben
+introducir en un array de 4 filas por 5 columnas. El programa mostrará las
+sumas parciales de filas y columnas igual que si de una hoja de cálculo se
+tratara. La suma total debe aparecer en la esquina inferior derecha
+ */
 public class Main {
     public static void main(String[] args) {
         //creamos y rellenamos el array
